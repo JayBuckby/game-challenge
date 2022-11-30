348be10eec418338abff7f989b7213630c5a5a86
Jay <JJ.Buckby@hotmail.co.uk>
2022-11-30 22:45:34 +0000

    Created start of match function

--- scripts/game.js
+++ scripts/game.js
@@ -8,7 +8,14 @@ const cardImage = document.querySelector(".card-image");
 const allCardValues = [1, 2, 3, 4, 5, 6, 7, 8];
 
 let cardImages = [];
+let userClickOne = "";
+let userClickTwo = "";
 
+const shuffleCards = (arr) => {
+  return arr.sort(function () {
+    return Math.random() - 0.5;
+  });
+};
 // run function to start game and populate grid
 
 const startGame = () => {
@@ -21,24 +28,35 @@ const startGame = () => {
     gameSquare += ".png";
     cardImages.push(gameSquare, gameSquare);
   });
-  //Shuffle the contents of the allCardValues array, to randomize the playability of game
 
-  //Render the grid, render the cards
-  for (i = 0; i < cardImages.length; i++) {
+  const shuffledCards = shuffleCards(cardImages);
+  for (i = 0; i < shuffledCards.length; i++) {
     const card = document.createElement("div");
     card.className = "gameCard";
-    card.setAttribute("id", i);
     card.innerHTML = `<div>
         <div class="blank">
-            <img class="card-image" id="${i}" src="./images/blankcard.png"</img>
+            <img src="./images/blankcard.png"</img>
         </div>        
     </div>`;
-    let temp = cardImages[i];
+    let imagePath = shuffledCards[i];
     card.addEventListener("click", (event) => {
-      console.dir(event.target.id);
-      card.innerHTML = `<div class="active">
-      <img src="${temp}"></img>
+      card.innerHTML = `<div id="${imagePath}" class="active">
+      <img src="${imagePath}"></img>
       </div>`;
+      if (userClickOne === "") {
+        userClickOne = imagePath;
+      } else if (userClickTwo === "") {
+        userClickTwo = imagePath;
+        if (userClickOne === userClickTwo) {
+          document.getElementById(imagePath).classList.add("match");
+          userClickOne = "";
+          userClickTwo = "";
+        } else {
+          alert("this is not a match");
+          userClickOne = "";
+          userClickTwo = "";
+        }
+      }
     });
     gameGrid.append(card);
   }
@@ -48,10 +66,10 @@ const restartGame = () => {
   restartButton.className = "hidden-button";
   startButton.className = "";
   gameGrid.innerHTML = "";
+  gameGrid.classList = "grid";
+  cardImages = [];
 };
 
 //EVENT LISTENERS
 startButton.addEventListener("click", startGame);
 restartButton.addEventListener("click", restartGame);
-
-// cardImage.addEventListener("click", handleBoxClick);
